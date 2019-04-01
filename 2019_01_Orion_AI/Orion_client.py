ca9b0eb19e42c61cca2a83d5e59d53a2a39e6a61
Thomassin-Rochon Vincent <1575290@decinfo.cvm>
2019-04-01 17:28:16 -0400

    working on all ide v2

--- 2019_01_Orion_AI/Orion_client.py
+++ 2019_01_Orion_AI/Orion_client.py
@@ -45,16 +45,9 @@ class Controleur():
 
     def creerpartie(self):
         if self.egoserveur==0:
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            print ("voici cwd v2   " + os.getcwd())
-            print("voici le directory   " + dir_path)
+            dir_path = os.path.dirname(os.path.realpath(__file__)) # chemin absolue du fichier ou il se trouve
 
-            if dir_path != os.getcwd():
-                directory = os.getcwd() + '/2019_01_Orion_AI/Orion_serveur.py'
-            else:
-                directory = dir_path + '/Orion_serveur.py'
-
-            pid = Popen([sys.executable, directory],shell=1).pid # A changer SELON L'IDE UTILISER
+            pid = Popen([sys.executable, dir_path + '/Orion_serveur.py'],shell=1).pid # A changer SELON L'IDE UTILISER
             self.egoserveur=1 # on note que c'est soi qui, ayant demarre le serveur, aura le privilege de lancer la simulation
             self.tempo=1 # on change d'etat pour s'inscrire automatiquement
                          # (parce que dans ce type de programme on prend pour acquis que celui qui prepare la simulation veut aussi y participer)
