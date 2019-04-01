a25d9242f4c69b3e687d4b804baa294c4f464c62
Miguel <miguel4696@hotmail.com>
2019-04-01 17:50:33 -0400

    Les structures arrivent

--- 2019_01_Orion_AI/Orion_modele.py
+++ 2019_01_Orion_AI/Orion_modele.py
@@ -1,17 +1,76 @@
  # -*- coding: utf-8 -*-   
 import random
+from math import ceil
 from Id import Id
 from helper import Helper as hlp
-        
+    
+
 class Planete():
     def __init__(self,x,y):
         self.id=Id.prochainid()
         self.proprietaire="inconnu"
         self.x=x
         self.y=y
-        self.taille=random.randrange(4,7) #taille des planetes
-        self.ressource=random.randrange(10)
-        
+        self.taille=random.randrange(4,6)
+        self.charbon=random.randrange(6)
+        self.zinc=random.randrange(5)
+        self.deuterium=random.randrange(10)
+        self.fertile=random.randrange(1)
+        self.listeStructure=[]*self.taille ## Chaque planète à une liste de bâtiments avec l'emplacement de chaque bâtiment
+
+
+    def estFertile(self):
+        return self.fertile
+
+    def creerStructure(self,x,y,nomStructure):
+        t=Structure(self,x,y,nomStructure)
+
+
+    def VendreStructure(self):
+        for i in self.listeStructure:
+            if i.select == True:
+                self.credit += ceil(i.creditTotal*)
+                self.listeStructure.remove(i)
+
+class Structure():
+    def __init__(self,nom,x,y,nomStructure):
+        self.proprietaire=nom
+        self.x=x
+        self.y=y
+        self.nomStructure=nomStructure
+
+        if nomStructure=="Usine_Civile":
+            self.vie=100
+            self.cout=150
+            self.maintenance=1
+            self.extraction=0
+        if nomStructure=="Usine_Militaire":
+            self.vie=200
+            self.cout=225
+            self.maintenance=2
+            self.extraction=0
+        if nomStructure=="Raffinerie_Diamant":
+            self.vie=80
+            self.cout=350
+            self.maintenance=6
+            self.extraction=2
+        if nomStructure=="Raffinerie_Charbon":
+            self.vie=50
+            self.cout=150
+            self.maintenance=2
+            self.extraction=3
+        if nomStructure=="Raffinerie_Isotope":
+            self.vie=175
+            self.cout=250
+            self.maintenance=3
+            self.extraction=2
+        if nomStructure=="Ferme":
+            self.vie=75
+            self.cout=50
+            self.maintenance=1
+            self.extraction=1
+
+
 class Vaisseau():
     def __init__(self,nom,x,y):
         self.id=Id.prochainid()
@@ -127,7 +186,8 @@ class Modele():
         self.terrain=[]
         self.creerplanetes(joueurs,2)
         self.creerterrain()
-        
+
+
     def creerterrain(self):
         self.terrain=[]
         for i in range(10):
